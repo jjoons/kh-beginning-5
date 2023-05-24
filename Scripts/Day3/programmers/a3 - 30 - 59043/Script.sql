d81518ebcef89caf13dcc3457e3eb83547c97b30
JJoonS <jjoons@jjoons.com>
2023-05-24 16:00:21 +0900

    0524 4

--- Scripts/Day3/programmers/a3 - 30 - 59043/Script.sql
+++ Scripts/Day3/programmers/a3 - 30 - 59043/Script.sql
@@ -1,1 +1,9 @@
 -- https://school.programmers.co.kr/learn/courses/30/lessons/59043
+
+SELECT
+    a.animal_id AS ANIMAL_ID,
+    a.name AS NAME FROM animal_ins a
+    JOIN animal_outs b ON a.animal_id = b.animal_id
+    WHERE a.datetime > b.datetime
+    ORDER BY a.datetime
+;
--- Scripts/Day3/programmers/a4 - 30 - 59042/Script.sql
+++ Scripts/Day3/programmers/a4 - 30 - 59042/Script.sql
@@ -1,1 +1,9 @@
 -- https://school.programmers.co.kr/learn/courses/30/lessons/59042
+
+SELECT
+    b.animal_id AS ANIMAL_ID,
+    b.name AS ANIMAL_NAME
+    FROM animal_ins a
+    RIGHT JOIN animal_outs b ON a.animal_id = b.animal_id
+    WHERE a.animal_id IS NULL
+    ;
